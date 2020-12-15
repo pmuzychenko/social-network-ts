9b0fc0a8513a672ffe48a7ac1408f2d19817cfab
pmuzychenko <pmuzychenko@gmail.com>
2020-12-15 16:48:41 +0200

    feat: add users' Navlink

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -17,6 +17,9 @@ export const Navbar = () => {
                 <NavLink to='/dialogs' activeClassName={styles.activeLink}>Messages</NavLink>
             </div>
             <div className={styles.item}>
+                <NavLink to='/users' activeClassName={styles.activeLink}>Users</NavLink>
+            </div>
+            <div className={styles.item}>
                 <NavLink to='/news' activeClassName={styles.activeLink}>News</NavLink>
             </div>
             <div className={styles.item}>
