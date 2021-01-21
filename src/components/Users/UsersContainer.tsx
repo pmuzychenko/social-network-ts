edf21597819728d4bddbec51b732015d70eb98b9
pmuzychenko <pmuzychenko@gmail.com>
2021-01-21 11:21:26 +0200

    feat: add Date Access Layer (DAL) to getting data from http request

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -9,6 +9,7 @@ import {
 import axios from "axios";
 import {Users} from "./Users";
 import {Preloader} from "../common/Preloader/Preloader";
+import {getUsers} from "../../api/api";
 
 type UsersPropsType = {
     follow: (id: number) => void
@@ -28,8 +29,7 @@ class UsersContainer extends React.Component<UsersPropsType> {
 
     componentDidMount() {
         this.props.toogleIsFetching(true)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,
-            {withCredentials: true}).then(response => {
+            getUsers(this.props.currentPage,this.props.pageSize).then(response => {
             this.props.toogleIsFetching(false)
             this.props.setUsers(response.data.items);
             this.props.setTotalUsersCount(response.data.totalCount);
@@ -39,7 +39,7 @@ class UsersContainer extends React.Component<UsersPropsType> {
     onPageChanged = (pageNumber: number) => {
         this.props.toogleIsFetching(true)
         this.props.setCurrentPage(pageNumber)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, {withCredentials: true}).then(response => {
+        getUsers(pageNumber,this.props.pageSize).then(response => {
             this.props.toogleIsFetching(false)
             this.props.setUsers(response.data.items);
         })
